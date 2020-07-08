a0251b074f1b6740aa86a6d255c577c8126f39bf
Billy Carlyle <billy@billycarlyle.uk>
2020-07-08 11:24:51 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BTS",
+    version="1.0",
+    author="Seamus Clarke",
+    description="Behind the Spectrum fitting code",
+    url="https://github.com/SeamusClarke/BTS",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'astropy',
+        'numpy',
+        'scipy',
+        'matplotlib'
+    ]
+)
